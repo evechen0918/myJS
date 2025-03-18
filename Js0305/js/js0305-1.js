a547af0c849a0b6cc61cbc0a283dc623657b2b49
evechen0918 <eveyes5428@gmail.com>
2025-03-18 16:13:44 +0800

    first

---
+++ Js0305/js/js0305-1.js
@@ -0,0 +1,5 @@
+document.write("hello, JavaScript");
+document.write('<br>'); //如果要放同一行註解，先tab then自己打//
+document.write('you are my love');
+document.write('<br>');
+document.write('no way');
---
+++ public/js01.js
@@ -0,0 +1,2 @@
+document.write('<br>');
+document.write('好多英文字母，頭好痛');
